15d9b65b10c1764e90ade4ce3a02880819720e44
Manogya Acharya <acharyma@bc.edu>
2020-09-02 10:51:05 -0400

    Week 1 Video Lesson Work Completed

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("👍 loaded")
     }
-
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        messageLabel.text = "Hello There"
+        messageLabel.textColor = UIColor.orange
+        print("General Kenobi ✩")
+    }
+    @IBAction func changeMessageAgainPressed(_ sender: UIButton) {
+        messageLabel.text = "Inspire Others!"
+        messageLabel.textColor = UIColor.blue
+        
+    }
+    
 
 }
 
