f8bf6b2f16f1a2fa678e2bbe8652c2becf456feb
Manogya Acharya <acharyma@bc.edu>
2020-09-02 10:26:12 -0400

    init

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Manogya Acharya on 9/2/20.
+//  Copyright © 2020 Manogya Acharya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
