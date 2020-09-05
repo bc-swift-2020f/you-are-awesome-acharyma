4d47f4c1279f33fd2ac7dc1849193e18929f4ac6
Manogya Acharya <acharyma@bc.edu>
2020-09-05 10:17:51 -0400

    2.8 completed

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -20,9 +20,23 @@ class ViewController: UIViewController {
         print("LOADDED")
     }
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        messageLabel.text = "You Are Awesome!"
-        messageLabel.textColor = UIColor.orange
-        imageView.image = UIImage(named: "image0")
+        let awesomeMessage = "You Are Awesome!"
+        let greatMessage = "You Are Great!"
+        let bombMessage = "You Are Da Bomb!"
+        
+        if messageLabel.text == awesomeMessage {
+            messageLabel.text = greatMessage
+            imageView.image = UIImage(named: "image1")
+        }
+        else if messageLabel.text == greatMessage {
+            messageLabel.text = bombMessage
+            messageLabel.textColor = UIColor.orange
+            imageView.image = UIImage(named: "image2")
+        }
+        else {
+            messageLabel.text = awesomeMessage
+            imageView.image = UIImage(named: "image0")
+        }
     }
     
 
